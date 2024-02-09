cdd72c47d1edf63df550083a346cccc2a4b9a89e
evimizak <89917763+evimizak@users.noreply.github.com>
2024-02-09 18:43:53 +0100

    Injected more weather data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,44 @@
 function refreshWeather (response) {
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = response.data.temperature.current;
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date (response.data.time * 1000); 
 
+    console.log(response.data)
+
+    timeElement.innerHTML = formatDate(date);
+
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
     temperatureElement.innerHTML = Math.round(temperature);
+    
+}
+
+function formatDate (date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+
 }
 
 
